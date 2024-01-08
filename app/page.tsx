ec477be03baa354a6d4af7205729d00fdf0160c5
Pranay Raj <masterpranayraj@gmail.com>
2024-01-09 01:25:33 +0530

    chore(ui): loader to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   return (
@@ -11,17 +15,43 @@ const Header = () => {
 };
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      setLoading(false);
+    };
+  }, []);
+
   return (
     <main className="w-full min-h-screen bg-slate-50 flex flex-col items-center p-16">
       <Header />
       <div className="rounded w-1/2 h-48 bg-white border border-gray-200 grid place-content-center">
-        <Button
+        {!loading && <Button
           className="bg-orange-600 hover:bg-orange-500 flex gap-2 items-center justify-center"
           size={"lg"}
+          asChild
+          onClick={() => setLoading(true)}
         >
-          <p>Try Instant Connect</p>
-          <ExternalLink />
-        </Button>
+          <Link href="/chat">
+            <p>Try Instant Connect</p>
+            <ExternalLink />
+          </Link>
+        </Button>}
+
+        {loading && (
+          <Button
+            className="bg-orange-600 hover:bg-orange-500 flex gap-2 items-center justify-center"
+            size={"lg"}
+            asChild
+            disabled
+          >
+            <span>
+              <p>Connecting</p>
+              <Loader2 className="animate-spin" />
+            </span>
+          </Button>
+        )}
       </div>
     </main>
   );
